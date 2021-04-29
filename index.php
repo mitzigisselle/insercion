e1abfba830198b50f098d7a4cddef45388fe2ad1
mitzigisselle <72286934+mitzigisselle@users.noreply.github.com>
2021-04-29 12:20:55 -0500

    version 1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insertar</title>
+</head>
+<body>
+    <form action="insertar.php" method="POST">
+        <label for="v1">valor 1</label>
+        <input type="text" name="v1" value="">
+        <br>
+        <label for="v2">valor 2</label>
+        <input type="text" name="v2" value="">
+        <br>
+        <input type="submit" name="btnGuardar" value="Guardar">
+    </form>
+</body>
+</html>
